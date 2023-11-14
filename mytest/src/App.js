e8e44357e3aa22c42e500e03012f67b4c9fec982
JoseFrags <josefrags240@gmail.com>
2023-11-14 15:55:04 +0100

    first commit

---
+++ mytest/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import FormExemple from './FormExemple';
+function App() {
+  return (
+    <div className="App">
+    <FormExemple/>
+    </div>
+  );
+}
+
+export default App;
